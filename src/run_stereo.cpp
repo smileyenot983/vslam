e75de71b6c7542fbba42c737835f336ef2f27518
ramil <ramilaidarovich@gmail.com>
2022-10-24 13:44:48 +0300

    feature extraction

--- src/run_stereo.cpp
+++ src/run_stereo.cpp
@@ -5,7 +5,8 @@
 
 #include "visual_odometry.cpp"
 
-
+#include <chrono>
+using namespace std::chrono;
 
 int main()
 {
@@ -17,9 +18,22 @@ int main()
     cv::Mat img_left, img_right;
 
 
-    int N_IMAGES = 20;
+    int N_IMAGES = 10;
     std::string pth_left, pth_right;
     
+    visualOdometry vo;
+
+
+    std::vector<cv::KeyPoint> cur_kpts_l;
+    cv::Mat cur_desc_l;
+    std::string write_name_l = "image_l";
+
+    std::vector<cv::KeyPoint> cur_kpts_r;
+    cv::Mat cur_desc_r;
+    std::string write_name_r = "image_r";
+
+    auto start = high_resolution_clock::now();
+
     for(int i=1; i<=N_IMAGES; i++)
     {
         pth_left = left_folder + "/" + std::to_string(i) + ".png";
@@ -30,11 +44,26 @@ int main()
         img_left = cv::imread(pth_left);
         img_right = cv::imread(pth_right);
 
-        cv::imwrite("left_image.png",img_left);
-        cv::imwrite("right_image.png",img_right);
+        // cv::imwrite("left_image.png",img_left);
+        // cv::imwrite("right_image.png",img_right);
 
+
+        vo.find_features(img_left, cur_kpts_l, cur_desc_r, true);
+        vo.find_features(img_right, cur_kpts_r, cur_desc_r, true);
+
+        std::cout << "n features(left): " << cur_kpts_l.size() << std::endl;
+        std::cout << "n features(right): " << cur_kpts_r.size() << std::endl;
+        
+        cv::imwrite(write_name_l + std::to_string(i) + ".png",img_left);
+        cv::imwrite(write_name_r + std::to_string(i) + ".png",img_right);
 
     }
+
+    auto end = high_resolution_clock::now();
+
+    auto duration = duration_cast<seconds>(end-start);
+
+    std::cout << "duration: " << duration.count() << std::endl;
 
 
 
--- src/visual_odometry.cpp
+++ src/visual_odometry.cpp
@@ -1,16 +1,60 @@
 #include <opencv2/opencv.hpp>
 
 #include <opencv2/features2d.hpp>
+// #include <opencv2/xfeatures2d.hpp>
+// #include "opencv2/nonfree/nonfree.hpp"
 
-
+#include <string>
 // processes pair of stereo images
 
-class VisualOdometry
+class visualOdometry
 {
-    // feature extractor(SIFT):
-    cv::Ptr<SIFT> sift_ptr;
 
+    private: 
+        // feature extractor(SIFT):
+        // cv::Ptr<cv::Feature2D> sift_ptr = cv::SIFT::create();
+
+        // cv::Ptr<cv::Feature2D> surf_ptr = cv::SURF::create(800.0);
+
+        cv::Ptr<cv::Feature2D> detector;
     
 
     // void addFrame
+
+    public:
+
+        visualOdometry(std::string detector="sift")
+        {   
+            if(detector=="sift")
+            {
+                detector = cv::SIFT::create();
+            }
+            else if(detector=="surf")
+            {
+                detector = cv::SURF::create(800.0);
+            }
+            else{
+                std::cout << "[ERROR] WRONG DETECTOR" << std::endl;
+            }
+        }
+
+
+        void find_features(cv::Mat &img, std::vector<cv::KeyPoint> &kpts, cv::Mat &desc, bool visualize = false)
+        {
+            detector->detectAndCompute(img, cv::Mat(), kpts, desc);
+
+            if(visualize)
+            {
+                draw_features(img, kpts);
+            }
+
+        }
+
+        void draw_features(cv::Mat &img, const std::vector<cv::KeyPoint> &kpts)
+        {
+            for(const auto &kp : kpts)
+            {
+                cv::circle(img, cv::Point(kp.pt.x,kp.pt.y),5,cv::Scalar(0,0,255),2);
+            }
+        }
 };
